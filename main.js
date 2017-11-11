8b3e2ac7916262183070016a63a298a3964e05ef
Stephen McGranahan <somtwo@gmail.com>
2017-11-11 16:24:20 -0600

    Make the bug happen

--- main.js
+++ main.js
@@ -1,60 +1,67 @@
 const electron = require('electron')
-// Module to control application life.
+
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const onWindows = process.platform === 'win32';
+
+if (onWindows) {
+	// Ignore dpi scaling
+	app.commandLine.appendSwitch('high-dpi-support', 'true');
+	app.commandLine.appendSwitch('force-device-scale-factor', 1.0);
+}
+
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  // This window will be an incorrect size (I'm seeing 800 x 620)
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+		useContentSize: true,
+		resizable: false,
+    show: false
+  });
 
-  // and load the index.html of the app.
+  if(onWindows) {
+    // Uncomment the next line and it will be a different (incorrect) size
+    // mainWindow.setSize(800, 600);
+  }
+
+  mainWindow.setMenu(null);
+
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.once('ready-to-show', () => {
+		mainWindow.show()
+	});
 
-  // Emitted when the window is closed.
+  mainWindow.webContents.openDevTools({
+    mode: 'detach'
+  });
+
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
